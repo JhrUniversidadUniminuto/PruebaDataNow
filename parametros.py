311acd3b0579e5d3b7526f73259298e0cfad5066
JhrUniversidadUniminuto <julian.herrera-r@uniminuto.edu.co>
2026-07-31 02:13:50 -0500

    Add files via upload

---
+++ parametros.py
@@ -0,0 +1,13 @@
+SERVER = "logitrackjulian2026pruebadatanow.database.windows.net"
+
+DATABASE = "LogiTrack_Transaccional"
+
+USER = "rootadmin"
+
+PASSWORD = "admin123*"
+
+DRIVER = "ODBC Driver 18 for SQL Server"
+
+SEED = 2026
+
+NUM_CONDUCTORES = 500
